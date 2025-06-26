e0588a866b952fdf839fc82a3dc1599a53321f14
Mzpenelope <phillipaaidoo12345@gmail.com>
2025-06-26 10:49:04 +0000

    all

--- index.php
+++ index.php
@@ -361,8 +361,9 @@ button:hover,
           <div class="slider-overlay"></div>
           <div class="slideshow">
             <div class="slider">
+              
               <div class="item"><img src="img/one.jpg" loading="lazy"/></div>
-              <div class="item"><img src="img/web/two.jpg" loading="lazy"/></div>
+                   <div class="item"><img src="img/web/two.jpg" loading="lazy"/></div>
               <div class="item"><img src="img/web/three.jpg" loading="lazy"/></div>
               <div class="item"><img src="img/web/four.jpg"loading="lazy" /></div>
             </div>
